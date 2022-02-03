a8b062fd5570779c952f190dc8727aa26be1b9ca
elmedina <eb45524@ubt-uni.net>
2022-02-03 16:00:00 +0100

    About

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>About</title>
+    <link rel="stylesheet" href="css\about.css">
+    <link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.7.0/css/all.css">
+</head>
+
+<body>
+
+    <div class="main-container">
+        
+        <?php
+        include 'topMenu.php';
+        ?>
+
+        <div class="container">
+
+            <div class="content">
+
+                <h1>About Us</h1>
+
+                <div class="story">
+                    <br>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati cupiditate corrupti excepturi
+                        dolores
+                        aspernatur, neque rem accusamus quam animi voluptates recusandae cumque nulla dicta nisi, autem
+                        quasi
+                        porro. Corrupti rem impedit placeat consequatur aperiam eligendi incidunt a sequi nisi animi,
+                        eius
+                        in
+                        non veritatis omnis aliquid labore ab! Quas accusantium odio ipsam eius facilis tenetur maxime
+                        assumenda
+                        laborum ducimus laboriosam odit ratione quisquam quam debitis, porro inventore voluptatem magni
+                        </p>
+
+                    <h6> FOUNDERS </h6>
+
+                    <div class="founders">
+                        <img id="founder" src="pictures/mu.jpg" alt="founder1">
+                        <img id="founder" src="pictures/mu2.jpg" alt="founder2">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <?php
+        include 'footer.php';
+        ?>
+
+    </div>
+
+</body>
+</html>
