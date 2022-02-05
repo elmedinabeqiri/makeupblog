0f787a3d4188d5f302e294aaff2f05747876da81
LoretaZeka <68351806+LoretaZeka@users.noreply.github.com>
2022-02-05 15:31:32 +0100

    reviews

---
+++ reviews.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reviews.css">
+    <link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <title>Reviews</title>
+</head>
+
+<body>
+    <div class="main-container">
+
+        <?php
+    
+    include 'topMenu.php';
+
+    ?>
+
+        <div class="container">
+            <div class="title">
+                <h1> Take a look about what our guests have been saying about us</h1> 
+                
+            </div>
+            <div class="content">
+              
+                    <div class="review">
+                        <div class="head-review">
+                            <img src="pictures/profile.jpg" width="250px" alt="">
+                        </div>
+                        <div class="body-review">
+                            <div class="name-review">Jane Doe</div>
+                            <div class="place-review">Lorem Ipsum</div>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <div class="desc-review">
+                              <p>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
+                                veniam, et
+                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
+                                Similique
+                                esse unde incidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+             
+             
+                    <div class="review">
+                        <div class="head-review">
+                            <img src="pictures/profile.jpg" width="250px" alt="">
+                        </div>
+                        <div class="body-review">
+                            <div class="name-review">Jane Doe</div>
+                            <div class="place-review">Lorem Ipsum</div>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <div class="desc-review">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
+                                veniam, et
+                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
+                                Similique
+                                esse unde incidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+               
+                
+                    <div class="review">
+                        <div class="head-review">
+                            <img src="pictures/profile.jpg" width="250px" alt="">
+                        </div>
+                        <div class="body-review">
+                            <div class="name-review">Jane Doe</div>
+                            <div class="place-review">Lorem Ipsum</div>
+                            <div class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half"></i>
+                            </div>
+                            <div class="desc-review">
+                             <p>   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam animi eligendi nesciunt
+                                veniam, et
+                                magnam eius amet quia nihil perspiciatis qui illum aut excepturi aperiam officiis.
+                                Similique
+                                esse unde incidunt.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+    </div>
+    <?php
+    
+    include 'footer.php';
+
+    ?>
+</body>
+
+</html>
